ff5868e430aacca704cfea051891fccb9377ec5e
IlyessBenRached <ilyesbenrached013@gmail.com>
2024-02-02 14:04:46 +0100

    moved secret key to secure env added links to footer component made changes to settings.py file

--- portfolio/grid/footer.js
+++ portfolio/grid/footer.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import Link from 'next/link'; // Import Link from Next.js
 import styles from '../components/footer.module.css';
 
 const ContactStack = () => {
   return (
-    <Stack className={styles.contact_stack} direction={{ xs: 'column', sm: 'row' }} spacing={2} >
+    <Stack className={styles.contact_stack} direction={{ xs: 'column', sm: 'row' }} spacing={2}>
       {/* Contact Information */}
-      <Stack className={styles.contact_info} alignItems={{ xs: 'center', sm: 'flex-start' }} spacing={2} >
+      <Stack className={styles.contact_info} alignItems={{ xs: 'center', sm: 'flex-start' }} spacing={2}>
         <Typography>Email: ilyesbenrached013@gmail.com</Typography>
         <Typography>Phone: +216 29 832 810</Typography>
       </Stack>
 
       {/* Links of the Website */}
       <Stack className={styles.website_links} alignItems={{ xs: 'center', sm: 'center' }} spacing={4}>
-        <Typography>Home</Typography>
-        <Typography>Education</Typography>
-        <Typography>Experience</Typography>
-        <Typography>Projects</Typography>
-        <Typography>Contact me</Typography>
-        <Typography>About Me</Typography>
+        <Typography>
+          <Link href="/" className={styles.link}>Home</Link>
+        </Typography>
+        <Typography>
+          <Link href="/education" className={styles.link}>Education</Link>
+        </Typography>
+        <Typography>
+          <Link href="/experience" className={styles.link}>Experience</Link>
+        </Typography>
+        {/* <Typography>
+          <Link href="/projects">Projects</Link>
+        </Typography> */}
+        <Typography>
+          <Link href="/contact" className={styles.link}>Contact me</Link>
+        </Typography>
+        <Typography>
+          <Link href="/about" className={styles.link}>About Me</Link>
+        </Typography>
       </Stack>
 
       {/* Social Icons */}
